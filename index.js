1834025c77727eb620bfdb6c2399de4c0fa78f44
Michael Mayer <michael@schnittstabil.de>
2015-03-09 17:45:58 +0100

    respect PATHEXT on Windows

--- index.js
+++ index.js
@@ -3,9 +3,16 @@ var gaze = require('gaze');
 var debounce = require('lodash.debounce');
 
 module.exports = function(pattern, cmd, args){
+  var opts = { stdio: 'inherit' };
+  if (process.platform === 'win32') {
+    args = ['/c', '"' + cmd + '"'].concat(args)
+    cmd = 'cmd'
+    opts.windowsVerbatimArguments = true
+  }
+
   function runner(event, filepath){
     console.log('Running: '+ cmd + ' ' + args.join(' '));
-    spawn(cmd, args, { stdio: 'inherit' });
+    spawn(cmd, args, opts);
   }
 
   gaze(pattern, function(err, watcher){
