1f353331aa2b8cd692f7f31cef5499b94442e057
Michael Mayer <michael@schnittstabil.de>
2018-10-28 20:02:04 +0100

    tweak tests

--- test.js
+++ test.js
@@ -5,91 +5,85 @@ import test from 'ava';
 const {exec} = childProcess;
 const CLI = './cli.js';
 
-function Listener() {
-	this.capture = {stderr: '', stdout: ''};
-	this.state = '';
+class Listener {
+	constructor(file, waitUntil) {
+		this.capture = {stderr: '', stdout: ''};
+		this.file = file;
+		this.waitUntil = waitUntil;
+		this.waitUntilTimer = null;
+		this.echoTimer = null;
+	}
+
+	spawnCli(...args) {
+		return new Promise((resolve, reject) => {
+			this.child = childProcess.spawn('node', [CLI].concat(args));
+			this.child.stderr.on('data', this.listen.bind(this, 'stderr'));
+			this.child.stdout.on('data', this.listen.bind(this, 'stdout'));
+			this.child.on('close', () => {
+				this.cleanUp();
+				resolve(this);
+			});
+			this.child.on('exit', () => {
+				this.cleanUp();
+				resolve(this);
+			});
+			this.child.on('error', err => {
+				this.cleanUp();
+				reject(err);
+			});
+			this.child.on('error', err => reject(err));
+		});
+	}
+
+	cleanUp() {
+		for (const timer of [this.echoTimer, this.waitUntilTimer]) {
+			if (timer) {
+				clearInterval(timer);
+			}
+		}
+	}
+
+	listen(stream, data) {
+		this.capture[stream] += String(data);
+		if (this.waitUntilTimer) {
+			return;
+		}
+		this.waitUntilTimer = setInterval(() => this.wait(), 100);
+		this.echoTimer = setInterval(() => this.echo(), 1000);
+	}
+
+	wait() {
+		if (!this.child.killed && this.waitUntil(this)) {
+			process.nextTick(() => this.child.kill('SIGKILL'));
+		}
+	}
+
+	echo() {
+		process.nextTick(() => exec(`echo "* { color: black }" >> ${this.file}`));
+	}
 }
 
-Listener.prototype.spawnCli = function (...args) {
-	return new Promise((resolve, reject) => {
-		this.sut = childProcess.spawn('node', [CLI].concat(args));
-		this.sut.stderr.on('data', this.listen.bind(this, 'stderr'));
-		this.sut.stdout.on('data', this.listen.bind(this, 'stdout'));
-		this.sut.on('close', resolve.bind(resolve, this));
-		this.sut.on('exit', resolve.bind(resolve, this));
-		this.sut.on('error', err => reject(err));
-	});
-};
-
-Listener.prototype.listen = function (stream, data) {
-	this.capture[stream] += String(data);
-	if (this.state === '') {
-		if (/Watching/.test(this.capture.stderr)) {
-			this.state = 'Watching';
-			this.onWatching();
-		}
-	}
-	if (this.state === 'Watching') {
-		if (/Running/.test(this.capture.stderr)) {
-			this.state = 'Running';
-		}
-	}
-	if (this.state === 'Running') {
-		this.onRunning();
-	}
-};
-
-Listener.prototype.close = function () {
-	this.state = 'Closing';
-	this.sut.kill('SIGKILL');
-};
-
-Listener.prototype.onWatching = () => {
-	process.nextTick(exec.bind(null, 'echo "* { color: black }" > fixtures/foo.less'));
-};
-
 test('watch files and run a command', async t => {
-	const listener = new Listener();
-
-	listener.onRunning = function () {
-		if (/^blorp$/m.test(this.capture.stdout)) {
-			this.close();
-		}
-	};
+	const listener = new Listener('fixtures/foo.less', self => /^blorp$/m.test(self.capture.stdout));
 
 	await listener.spawnCli('--pattern', 'fixtures/*.less', '--', 'echo', 'blorp');
 
 	t.is(listener.capture.stdout.trim(), 'blorp');
-	t.is(listener.state, 'Closing');
 });
 
 test('output exit code in verbose mode', async t => {
-	const listener = new Listener();
+	const listener = new Listener('fixtures/foo.sass', self => /exited/m.test(self.capture.stderr));
 
-	listener.onRunning = function () {
-		if (/exited/m.test(this.capture.stderr)) {
-			this.close();
-		}
-	};
-
-	await listener.spawnCli('-v', '--pattern', 'fixtures/*.less', '--', 'echo', 'blorp');
+	await listener.spawnCli('-v', '--pattern', 'fixtures/*.sass', '--', 'echo', 'blorp');
 
 	const stderr = listener.capture.stderr.trim();
 	t.regex(stderr, /exited with code 0$/m);
-	t.is(listener.state, 'Closing');
 });
 
 test('handle mixed quotations of diffrent types correctly', async t => {
-	const listener = new Listener();
+	const listener = new Listener('fixtures/foo.css', self => /^blorp$/m.test(self.capture.stdout));
 
-	listener.onRunning = function () {
-		if (/^blorp$/m.test(this.capture.stdout)) {
-			this.close();
-		}
-	};
-
-	await listener.spawnCli('--pattern', 'fixtures/*.less', '--', 'node', '-e', 'console.log(\'blorp\');');
+	await listener.spawnCli('--pattern', 'fixtures/*.css', '--', 'node', '-e', 'console.log(\'blorp\');');
 
 	t.is(listener.capture.stdout.trim(), 'blorp');
-	t.is(listener.state, 'Closing');
 });
